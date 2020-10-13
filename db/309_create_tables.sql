75a5990cc1e40b61a89c3c982daa273ced56817b
PhilCAEUser <rbyETE4Ceu2iA4Z>
2020-10-13 12:00:52 +0000

    Test

---
+++ db/309_create_tables.sql
@@ -0,0 +1,16 @@
+--
+-- Database Schema:  caeschema
+-- Automatically generated sql script for the service 309, created by the CAE.
+-- --------------------------------------------------------
+
+--
+-- Table structure for table exp.
+--
+CREATE TABLE caeschema.exp (
+  exp_col INTEGER ,
+CONSTRAINT exp_col_PK PRIMARY KEY (exp_col)
+ 
+);
+
+
+
